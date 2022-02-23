9d171fb4d581f816af1b6e1607afb42be7df26b9
AlyssiaPH <alyssia0207@gmail.com>
2022-02-23 23:02:28 +0100

    working ?

--- src/scanner.js
+++ src/scanner.js
@@ -3,6 +3,7 @@ import QrScanner from 'qr-scanner';
 import hmacSHA512 from 'crypto-js/hmac-sha512';
 import QRCode from "qrcode.react";
 import React from "react";
+var CryptoJS = require("crypto-js");
 
 let qrScanner;
 
@@ -29,12 +30,15 @@ function stopScan(){
 }
 
 function ScannerDiv() {
-    const diarre = hmacSHA512("Message", "Key");
-    console.log(diarre)
+    const diarre = CryptoJS.AES.encrypt("coucouc la streat", "keyultrasecure");
+    const descryptDiarre = CryptoJS.AES.decrypt(diarre, "keyultrasecure").toString(CryptoJS.enc.Utf8);
+    console.log(diarre, descryptDiarre);
+
     return (
         <div>
             <QRCode value={diarre.toString()} />
             <p>{diarre.toString()}</p>
+            <p>{descryptDiarre.toString()}</p>
             <button id="startScanBtn" onClick={() => {startScan()}}>
                 Start</button>
             <button id="stopScanBtn" style={{visibility:'hidden'}} onClick={() => {stopScan()}}>
