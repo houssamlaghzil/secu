c77c85115e6490d0877d0da9fdbb75eab72078d3
AlyssiaPH <alyssia0207@gmail.com>
2022-02-23 21:37:50 +0100

    stop when qr code found

--- src/scanner.js
+++ src/scanner.js
@@ -5,13 +5,21 @@ let qrScanner;
 //let visibilityStart = "visible";
 //let visibilityStop = "hidden";
 
+function getQrCodeData(data){
+    console.log("Data of the Qr Code", data)
+    stopScan();
+}
+
 function startScan(){
     document.getElementById("startScanBtn").style.visibility = "hidden";
     document.getElementById("stopScanBtn").style.visibility = "visible";
     let videoElem = document.getElementById("videoDiv");
-    qrScanner = new QrScanner(videoElem, result => console.log('decoded qr code:', result));
-    qrScanner.start().then(r =>
-    console.log(r));
+    qrScanner = new QrScanner(videoElem, result => {
+        getQrCodeData(result)
+    });
+    qrScanner.start().then(r => {
+        console.log("Scanner started");
+    });
 }
 function stopScan(){
     document.getElementById("startScanBtn").style.visibility = "visible";
